266d141f7d7ce0048cbb086289e318724bc38934
Jofre Dev <gavidiajofreeduardo@gmail.com>
2024-05-15 18:50:09 -0500

    se añade cliente webflux al proyecto

---
+++ webflux-client/src/test/java/com/reactive/functionalendpoints/FunctionalEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.functionalendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
